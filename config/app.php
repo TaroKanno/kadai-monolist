a3f6674ddcf35cbfb09c339d3e1809fb88eb856a
Taro Kanno <taro1225.enjoy@icloud.com>
2018-01-31 03:58:58 +0000

    user login

--- config/app.php
+++ config/app.php
@@ -143,6 +143,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
     ],
 
@@ -193,6 +194,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
