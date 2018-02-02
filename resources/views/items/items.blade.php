f03c9ce793ebd91167fec44e131d8f5e5161569d
Taro Kanno <taro1225.enjoy@icloud.com>
2018-02-02 10:14:11 +0000

    have ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,11 +1,11 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
                         <div class="panel-heading text-center">
-                            <img src="{{ $item->image_url }}" alt="">
+                            <img src="{{ $item->image_url }}" alt="" class="">
                         </div>
                         <div class="panel-body">
                             @if ($item->id)
@@ -14,7 +14,7 @@
                                 <p class="item-title">{{ $item->name }}</p>
                             @endif
                             <div class="buttons text-center">
-                               @if (Auth::check())
+                                @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
                                     @include('items.have_button', ['item' => $item])
                                 @endif
